a17cdd162a394af3e51069da372e7c30df9f923b
cchura94 <cchura.cpc@gmail.com>
2021-06-07 19:30:22 -0400

    Listar y Guardar Categoria

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ let router = express.Router();
 import {verificaAuth} from "./../middlewares/authMiddleware"
 
 import * as authController from "./../controllers/authController.js"
-
+import * as catController  from "./../controllers/categoriaController"
 
 
 // Rutas Autenticación
@@ -12,6 +12,9 @@ router.post("/auth/login2", authController.ingresar2);
 
 router.post("/registro", verificaAuth, authController.registroUsuario2);
 
+// rutas de Categoria
+router.get("/categoria", catController.lista);
+router.post("/categoria", catController.guardar);
 
 // module.exports = router;
 export default router;
