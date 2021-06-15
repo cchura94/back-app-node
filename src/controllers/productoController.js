4b709b5e2f131da0a1728b179d8ee13cf5e934a4
cchura94 <cchura.cpc@gmail.com>
2021-06-15 12:52:57 -0400

    Guardar Producto

--- src/controllers/productoController.js
+++ src/controllers/productoController.js
@@ -1,9 +1,19 @@
 import models from "./../models"
 
 export const lista = async function(req, res){
+
+    // 127.0.0.1:3000/api/producto?page=1&limit=20
     try{
         // realizar Busquedas
-        const datos = await models.Producto.findAll();
+        // Paginación
+        let page = req.query.page;
+        let limit = req.query.limit;
+        let offset = 0 + (page - 1) * limit
+        const datos = await models.Producto.findAndCountAll({
+            limit: limit,
+            offset: offset,
+            
+        });
         res.json(datos);
     }catch(error){
         res.status(500).send({
@@ -12,18 +22,38 @@ export const lista = async function(req, res){
     }
 }
 
-export const guardar = function(req, res){
+export const guardar = async function(req, res){
     // validar
-    // subir imagen
-    if(req.file){
-        console.log(req.file.filename)
-        res.send("Tiene Imagenes...");
+    let { nombre, cantidad } = req.body;
+    if(typeof nombre != String && !nombre){
+        res.status(400).send({
+            error: true,
+            mensaje: "el campo nombre no deberia estar vacio y debe ser Cadena"
+        })
     }else{
-        res.send("No Tiene Imagenes...");
+        try{
+            // subir imagen
+            if(req.file){
+                console.log(req.file.filename)
+                req.body.imagen = req.file.filename;
+            }
+            // guardar
+        
+            let data = await models.Producto.create(req.body);
+            // responder
+            res.json({
+                mensaje: "Producto Registrada",
+                data: data,
+                error: false
+            });
+        }catch(error){
+            res.status(500).send({
+                error: true,
+                mensaje: error.message || 'Error al guardar en la base de datos'
+            })
+        }
+
     }
-    // guardar
-    // responder
-    res.send("Imagen subida...");
 }
 
 export const mostrar = function(req, res){
